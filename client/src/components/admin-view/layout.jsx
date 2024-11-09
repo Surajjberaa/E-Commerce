6ff027e4a4b451545cc77184fb7ac3fa97018647
Suraj Bera <berasuraj08@gmail.com>
2024-11-10 01:51:26 +0530

    Routing Added

---
+++ client/src/components/admin-view/layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+function AdminLayout() {
+  return (
+    <div className='flex min-h-screen w-full'>
+      Admin sidebar
+      <div className='flex flex-1 flex-col '>
+        {/* Admin Header */}
+        <main className='flex flex-1 bg-muted/40 p-4 md:p-6 '>
+            <Outlet/>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default AdminLayout
---
+++ client/src/components/shopping-view/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShoppingHeader() {
+  return (
+    <div className=''>
+        Shopping view Header 
+    </div>
+  )
+}
+
+export default ShoppingHeader
