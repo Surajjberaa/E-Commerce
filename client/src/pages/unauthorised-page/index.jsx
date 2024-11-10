64b80dff04972fb8af8dfb7efee8789c55b079eb
Suraj Bera <berasuraj08@gmail.com>
2024-11-10 16:17:02 +0530

    Auth routing logic implementation

---
+++ client/src/pages/unauthorised-page/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UnauthorisedPage() {
+  return (
+    <div>
+
+    <h1>Lost Your Path Buddy?</h1>
+    <h2>You Don't Have Access To This Page</h2>
+    </div>
+  )
+}
+
+export default UnauthorisedPage
