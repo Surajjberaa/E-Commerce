e18ded120d89e1b0a65d17695b3e04fc1f445dba
Suraj Bera <berasuraj08@gmail.com>
2024-11-21 17:13:01 +0530

    listing view frontend layout cpmpleted

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,8 +1,49 @@
+import ProductFilter from '@/components/shopping-view/filter'
+import { Button } from '@/components/ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { sortOptions } from '@/config'
+import { ArrowUpDownIcon } from 'lucide-react'
 import React from 'react'
 
 function ShoppingListing() {
   return (
-    <div>ShoppingListing</div>
+    <div className='grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6 '>
+      <ProductFilter />
+      <div className='bg-background w-full rounded-lg shadow-sm '>
+        <div className='p-4 border-b flex item-center justify-between'>
+          <h2 className='text-lg font-semibold'>
+            All Products
+          </h2>
+          <div className='flex items-center gap-4'>
+            <span className=' text-muted-foreground'>
+              Showing 1 to 10 of 100 products
+            </span>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button className='flex items-center gap-2' variant='outline' size='sm'>
+                  <ArrowUpDownIcon className='w-4 h-4' />
+                  <span>
+                    Sort By
+                  </span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align='end' className='w-[200]'>
+                <DropdownMenuRadioGroup>
+                  {
+                    sortOptions.map((option) => (
+                      <DropdownMenuRadioItem key={option.id}>
+                        {option.label}
+                      </DropdownMenuRadioItem>
+                    ))
+                  }
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
