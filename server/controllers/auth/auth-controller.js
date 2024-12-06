b708d7214054816b59c3676b1fd926d2e7d8fcc0
Suraj Bera <berasuraj08@gmail.com>
2024-12-07 05:04:39 +0530

    removed sameSite

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -78,7 +78,7 @@ export const loginUser = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: true,
-            sameSite: 'None',
+            sameSite: 'Strict',
         }).json({
             success: true,
             message: "Logged In Successfuly",
